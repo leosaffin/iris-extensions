891bc1a1c26d65116089886b6f3ab1535d20e540
Leo Saffin <string_buster@hotmail.com>
2022-02-11 14:15:10 +0000

    Add extra function for contour diagnostics from using for cold pools

--- irise/diagnostics/contours.py
+++ irise/diagnostics/contours.py
@@ -1,3 +1,5 @@
+from shapely.geometry.polygon import Polygon
+
 import numpy as np
 
 import iris.plot as iplt
@@ -9,6 +11,58 @@ def get_contours(evaporation_tracer, threshold=1e-4):
     return cs.allsegs[0]
 
 
+def as_array(polygons):
+    return np.array(polygons[0].exterior.coords)
+
+
+def filter_invalid(contours, maximum_distance):
+    """Remove contours that are not closed or contained within other contours
+
+    Args:
+        contours:
+        maximum_distance:
+
+    Returns:
+
+    """
+    # Sort contours by distance
+    contours = sorted([contour for contour in contours if len(contour) > 2], key=contour_length)
+    polygons = [Polygon(contour) for contour in contours]
+
+    # Check whether the shorter contours are contained by the longer contours
+    to_be_removed = []
+    for n, poly1 in enumerate(polygons):
+        for m, poly2 in enumerate(polygons):
+            if n != m and poly2.contains(poly1):
+                to_be_removed.append(n)
+
+    # Remove open contours
+    for n, contour in enumerate(contours):
+        if not is_closed(contour, threshold=maximum_distance):
+            to_be_removed.append(n)
+
+    for n in reversed(sorted(set(to_be_removed))):
+        del contours[n]
+
+    return contours
+
+
+def contour_length(points):
+    """Contour length in kilometres
+
+    Args:
+        points: Nx2 array of longitude and latitude points around a contour (degrees)
+
+    Returns:
+        float: The total length of the contour in kilometres
+    """
+    conlen = haversine(points[-1], points[0])
+    for n in range(len(points) - 1):
+        conlen += haversine(points[n], points[n+1])
+
+    return conlen
+
+
 def is_closed(contour, threshold):
     """Checks that a contour is closed
 
@@ -16,7 +70,7 @@ def is_closed(contour, threshold):
     initial point on a countour to determine if it is closed
 
     Args:
-        contour_section (np.Array):
+        contour (np.Array):
             An array of coordinates for each point along the contour of shape
             (N, 2).
 
